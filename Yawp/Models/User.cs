d259be7cc1658b8bc7c41358e4544d7c96ec9ff8
Nathan Lee <nathanlee@aggienetwork.com>
2021-01-11 19:30:30 -0600

    Close #1 - basic User class created

---
+++ Yawp/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yawp.Models
+{
+    public class User
+    {
+        public User(string firstName, string lastName, string username)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Username = username;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Username { get; set; }
+
+        public Contact LinkedContact { get; set; }
+
+        public int Id { get; }
+
+    }
+}
