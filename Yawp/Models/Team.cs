cfb31dc50a9db65af64eddb1b27f1ca4bcaab966
Nathan Lee <nathanlee@aggienetwork.com>
2021-01-11 21:17:02 -0600

    Added Members property to Team

--- Yawp/Models/Team.cs
+++ Yawp/Models/Team.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Yawp.Models
 {
     public class Team
@@ -20,6 +21,8 @@ namespace Yawp.Models
         public User Owner { get; set; }
 
         public List<User> Managers { get; set; }
+
+        public List<Contact> Members { get; set; }
 
         public int Id { get; }
     }
