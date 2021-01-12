fdc6ca478f2e870e381534ff6911daaed59497b6
Nathan Lee <nathanlee@aggienetwork.com>
2021-01-11 19:35:31 -0600

    Close #2 - created Contact class

---
+++ Yawp/Models/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yawp.Models
+{
+    public class Contact
+    {
+        public Contact(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Description { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public User LinkedUser { get; set; }
+
+        public int Id { get; }
+
+    }
+}
