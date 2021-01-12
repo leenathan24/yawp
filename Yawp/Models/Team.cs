a7bb393d124af9bf7e5c82e708fb090824890bb9
Nathan Lee <nathanlee@aggienetwork.com>
2021-01-11 19:36:44 -0600

    Close #3 - created basic Team class

---
+++ Yawp/Models/Team.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yawp.Models
+{
+    public class Team
+    {
+        public Team(string name, User owner)
+        {
+            Name = name;
+            Owner = owner;
+        }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public User Owner { get; set; }
+
+        public List<User> Managers { get; set; }
+
+        public int Id { get; }
+    }
+}
