692e4538a551245a80a99604f07453249f1011f5
Nathan Lee <nathanlee@aggienetwork.com>
2021-01-14 22:49:58 -0600

    #5 - Remove Contact.OwnedBy from Create/Edit

--- Yawp/Controllers/ContactsController.cs
+++ Yawp/Controllers/ContactsController.cs
@@ -54,7 +54,7 @@ namespace Yawp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Description,EmailAddress,PhoneNumber,OwnedBy")] Contact contact)
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Description,EmailAddress,PhoneNumber")] Contact contact)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace Yawp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Description,EmailAddress,PhoneNumber,OwnedBy")] Contact contact)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Description,EmailAddress,PhoneNumber")] Contact contact)
         {
             if (id != contact.Id)
             {
