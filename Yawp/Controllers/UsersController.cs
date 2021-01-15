d6f78e319018251631f992e1340eae334e01822c
Nathan Lee <nathanlee@aggienetwork.com>
2021-01-14 22:31:32 -0600

    Remove User.LinkedContact from Create/Edit pages

--- Yawp/Controllers/UsersController.cs
+++ Yawp/Controllers/UsersController.cs
@@ -54,7 +54,7 @@ namespace Yawp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,PhoneNumber,EmailAddress,Username,LinkedContact")] User user)
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,PhoneNumber,EmailAddress,Username")] User user)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace Yawp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,PhoneNumber,EmailAddress,Username,LinkedContact")] User user)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,PhoneNumber,EmailAddress,Username")] User user)
         {
             if (id != user.Id)
             {
